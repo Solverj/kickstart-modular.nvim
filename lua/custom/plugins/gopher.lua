32122ed6110ef469f69bb3f5be6b6a9058da8924
solve sthor <solve.rene.sthor@mattilsynet.no>
2024-01-12 13:23:00 +0100

    removed colors from debug, added gopher for :go commands.

---
+++ lua/custom/plugins/gopher.lua
@@ -0,0 +1,15 @@
+return {
+  "olexsmir/gopher.nvim",
+  ft = "go",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-lua/popup.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  config = function(_, opts)
+    require("gopher").setup(opts)
+  end,
+  build = function()
+    vim.cmd([[silent! GoInstallDeps]])
+  end,
+}
