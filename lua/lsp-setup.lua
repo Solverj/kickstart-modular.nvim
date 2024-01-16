bb165788ec3764df6d3808a4b91ddc1e7845858d
sølve <solve.sthor@gmail.com>
2024-01-16 23:42:32 +0100

    added undotree and clipboard-history.

--- lua/lsp-setup.lua
+++ lua/lsp-setup.lua
@@ -1,6 +1,10 @@
 -- [[ Configure LSP ]]
 --  This function gets run when an LSP connects to a particular buffer.
-local on_attach = function(_, bufnr)
+local on_attach = function(client, bufnr)
+  if client.server_capabilities.inlayHintProvider then
+    vim.lsp.inlay_hint.enable(bufnr, true)
+    -- vim.api.nvim_set_hl(0, 'LspInlayHint', { bg = '#191970' })
+  end
   --  vim.lsp.inlay_hint.enable(bufnr, true)
   -- NOTE: Remember that lua is a real programming language, and as such it is possible
   -- to define small helper and utility functions so you don't have to repeat yourself
